83c4ba9d4b2bb7bc08801cfbab520eb6e4d6093f
nickthorpe71 <nickthorpe71@gmail.com>
2020-12-21 15:15:01 -0600

    implements basic api with GET and POST for a user

--- src/main/java/com/los/demo/dao/PersonDao.java
+++ src/main/java/com/los/demo/dao/PersonDao.java
@@ -2,15 +2,17 @@ package com.los.demo.dao;
 
 import com.los.demo.model.Person;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PersonDao {
 
     int insertPerson(UUID id, Person person);
 
-    default int addPerson(Person person) {
+    default int insertPerson(Person person) {
         UUID id = UUID.randomUUID();
         return insertPerson(id, person);
     }
 
+    List<Person> selectAllPeople();
 }
---
+++ src/main/java/com/los/demo/service/PersonService.java
@@ -0,0 +1,28 @@
+package com.los.demo.service;
+
+import com.los.demo.dao.PersonDao;
+import com.los.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service // Same as @Component but more semantic
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        return personDao.insertPerson(person);
+    }
+
+    public List<Person> getAllPeople(){
+        return personDao.selectAllPeople();
+    }
+}
