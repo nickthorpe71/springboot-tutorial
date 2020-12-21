6601dc367d8914478b00ecec842c211fcf4f0699
nickthorpe71 <nickthorpe71@gmail.com>
2020-12-21 14:30:31 -0600

    init commit

---
+++ src/main/java/com/los/demo/dao/PersonDao.java
@@ -0,0 +1,16 @@
+package com.los.demo.dao;
+
+import com.los.demo.model.Person;
+
+import java.util.UUID;
+
+public interface PersonDao {
+
+    int insertPerson(UUID id, Person person);
+
+    default int addPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
+}
